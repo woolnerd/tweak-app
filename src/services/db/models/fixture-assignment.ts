41d992cb6c55afbe9e1b7d9410141db19ee6604e
Dave Woolner <davidwoolner@gmail.com>
2024-03-19 12:27:56 -0400

    Save fixture assignment model

--- src/services/db/models/fixture-assignment.ts
+++ src/services/db/models/fixture-assignment.ts
@@ -1,14 +1,35 @@
 import { PrismaClient, FixtureAssignment, Prisma } from '@prisma/client';
 const prisma = new PrismaClient();
 
+interface GetAllOptions {
+  scenes?: boolean;
+  fixture?: boolean;
+  profile?: boolean;
+}
+
 export async function createFixtureAssignment(
   fixtureAssignment: Prisma.FixtureAssignmentCreateInput
 ) {
   return await prisma.fixtureAssignment.create({ data: fixtureAssignment });
 }
 
-export async function getAllFixtureAssignments() {
-  return await prisma.fixtureAssignment.findMany();
+export async function getAllFixtureAssignments(options: GetAllOptions) {
+  return await prisma.fixtureAssignment.findMany({ include: options });
+}
+
+export async function getFixtureAssignment(fixtureAssignmentId: number) {
+  return await prisma.fixtureAssignment.findUnique({
+    where: { id: fixtureAssignmentId },
+  });
+}
+
+export async function updateFixtureAssignment(
+  fixtureAssignment: FixtureAssignment
+) {
+  return await prisma.fixtureAssignment.update({
+    where: { id: fixtureAssignment.id },
+    data: fixtureAssignment,
+  });
 }
 
 export async function deleteFixtureAssignment(id: number) {
