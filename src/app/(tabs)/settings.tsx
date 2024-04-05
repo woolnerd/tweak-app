f22a0641b84fe7679e414871bf64722fdf809ba6
woolnerd <davidwoolner@gmail.com>
2024-04-04 21:45:57 -0400

    save patch page

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -5,21 +5,27 @@ export default function Tab() {
   const UNIVERSE_SIZE = 512;
 
   const buildUniverse = () => {
-    const universe: React.JSX.Element[][] = [];
+    const universe: React.JSX.Element[] = [];
+    let row: React.JSX.Element[] = [];
     for (let i = 1; i <= UNIVERSE_SIZE; i++) {
-      const row: React.JSX.Element[] = [];
-      for (let j = 1; j <= 10; j++) {
-        row.push(<View style={{width: 10, height: 10, backgroundColor: 'gold', margin: 2}} />);
-        if (j == 10) {
-          universe.push(row);
-        }
+      row.push(<View key={i} style={{ width: 25, height: 25, backgroundColor: 'gold', margin: 2, borderColor:'red', borderWidth: 1}}><Text>{i}</Text></View>);
+      if (i % 10  === 0) {
+        universe.push(
+          <View key={`row` + i} style={{ width: 'auto', justifyContent: 'space-evenly'}}>
+              {row}
+          </View>
+        );
+        row = []
       }
     }
+    return universe;
   }
 
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Tab Settings</Text>
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <View style={{height: 600, flexDirection: 'row'}}>
+        {buildUniverse()}
+      </View>
     </View>
   );
 }
