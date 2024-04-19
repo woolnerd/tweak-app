ccf5e4a750a98cb0f7ab16669418ff011b78659e
woolnerd <davidwoolner@gmail.com>
2024-04-19 16:41:28 -0400

    add command line hook

--- src/app/hooks/use-command-line-router.ts
+++ src/app/hooks/use-command-line-router.ts
@@ -1,3 +1,48 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
-export function useCommandLineRouter() {}
+import ProfileAdapter from "../../lib/adapters/profile-adapter.ts";
+import { ActionObject } from "../../lib/command-line/types/command-line-types.ts";
+import ValueRouter from "../../lib/value-router.ts";
+import { ParsedCompositeFixtureInfo } from "../../models/types/scene-to-fixture-assignment.ts";
+import { useCompositeFixtureStore } from "../store/useCompositeFixtureStore.ts";
+
+export default function useCommandLineRouter(action: ActionObject | null) {
+  const compositeFixtures = useCompositeFixtureStore(
+    (state) => state.compositeFixtures,
+  );
+  const updateCompositeFixtures = useCompositeFixtureStore(
+    (state) => state.updateCompositeFixtures,
+  );
+
+  useEffect(() => {
+    if (action !== null && action.complete) {
+      const { selection } = action;
+      console.log("compFixtures", compositeFixtures);
+
+      const selectedCompositeFixtures = compositeFixtures.filter((fixture) =>
+        selection.includes(fixture.channel),
+      );
+      console.log("selectedCompFix", selectedCompositeFixtures);
+
+      const mutatedFixtures = selectedCompositeFixtures.map((fixture) => {
+        const profileAdapter = new ProfileAdapter(
+          action.profileTarget,
+          fixture.profileChannels!,
+        );
+
+        const valueRouter = new ValueRouter<ParsedCompositeFixtureInfo>(
+          action,
+          profileAdapter,
+        );
+
+        valueRouter.buildResult().mutateOrMergeFixtureChannels(fixture);
+
+        return fixture;
+      });
+      updateCompositeFixtures(mutatedFixtures);
+    }
+
+    console.log("action", action);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [action, updateCompositeFixtures]);
+}
--- src/components/control-panel.tsx
+++ src/components/control-panel.tsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { View } from "react-native";
 
 import ControlPanelButton from "./control-panel-button.tsx";
-import { useCompositeFixtureStore } from "../app/store/useCompositeFixtureStore.ts";
+import useCommandLineRouter from "../app/hooks/use-command-line-router.ts";
 import controlPanelButtonData from "../db/button-data.ts";
-import ProfileAdapter from "../lib/adapters/profile-adapter.ts";
 import CommandLine from "../lib/command-line/command-line.ts";
 import { ActionObject } from "../lib/command-line/types/command-line-types.ts";
 import { ControlButton } from "../lib/types/buttons.ts";
-import ValueRouter from "../lib/value-router.ts";
-import { ParsedCompositeFixtureInfo } from "../models/types/scene-to-fixture-assignment.ts";
 
 type ControlPanelProps = {
   setControlPanelValue: any;
@@ -18,12 +15,6 @@ export default function ControlPanel({
   setControlPanelValue,
 }: ControlPanelProps) {
   const [action, setAction] = useState<ActionObject | null>(null);
-  const compositeFixtures = useCompositeFixtureStore(
-    (state) => state.compositeFixtures,
-  );
-  const updateCompositeFixtures = useCompositeFixtureStore(
-    (state) => state.updateCompositeFixtures,
-  );
 
   const handleTouch = (data: ControlButton) => {
     const commandLineInstance = CommandLine.getInstance();
@@ -31,37 +22,7 @@ export default function ControlPanel({
     setAction(commandLineAction);
   };
 
-  useEffect(() => {
-    if (action !== null && action.complete) {
-      const { selection } = action;
-      console.log("compFixtures", compositeFixtures);
-
-      const selectedCompositeFixtures = compositeFixtures.filter((fixture) =>
-        selection.includes(fixture.channel),
-      );
-      console.log("selectedCompFix", selectedCompositeFixtures);
-
-      const mutatedFixtures = selectedCompositeFixtures.map((fixture) => {
-        const profileAdapter = new ProfileAdapter(
-          action.profileTarget,
-          fixture.profileChannels!,
-        );
-
-        const valueRouter = new ValueRouter<ParsedCompositeFixtureInfo>(
-          action,
-          profileAdapter,
-        );
-
-        valueRouter.buildResult().mutateOrMergeFixtureChannels(fixture);
-
-        return fixture;
-      });
-      updateCompositeFixtures(mutatedFixtures);
-    }
-
-    console.log("action", action);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [action, updateCompositeFixtures]);
+  useCommandLineRouter(action);
 
   const buildPanel = () =>
     controlPanelButtonData.map((col) => (
