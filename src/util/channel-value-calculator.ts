f2258a43384b6934de6e07d7788aa9be61f0e57f
woolnerd <davidwoolner@gmail.com>
2024-04-19 14:30:19 -0400

    change calc to handle integers, and pass tests

--- src/util/channel-value-calculator.ts
+++ src/util/channel-value-calculator.ts
@@ -2,21 +2,35 @@
 export default class ChannelValueCalculator {
   private percentage: number;
 
-  private FULL_DMX_VALUE = 256 * 256;
+  private FULL_DMX_VALUE = 65535;
+
+  private readonly calcPercentOfFullDMXVal: number;
+
+  private readonly coarseValue: number;
+
+  private readonly fineValue: number;
 
   constructor(percentage: number) {
     this.percentage = percentage;
+    this.boundsCheck();
+    this.calcPercentOfFullDMXVal = Math.round(
+      (this.FULL_DMX_VALUE * this.percentage) / 100,
+    );
+    this.coarseValue = this.calcPercentOfFullDMXVal / 256;
+    this.fineValue = this.calcPercentOfFullDMXVal % 256;
   }
 
   public calc16BitValues(): number[] {
-    return [this.getCoarseValue(), this.getFineValue()];
+    return [Math.trunc(this.coarseValue), Math.trunc(this.fineValue)];
   }
 
-  public getCoarseValue() {
-    return Math.floor((this.FULL_DMX_VALUE * this.percentage) / 256);
-  }
+  private boundsCheck() {
+    if (this.percentage < -100) {
+      throw new Error("Percentage cannot be less than -100");
+    }
 
-  private getFineValue() {
-    return this.getCoarseValue() & 255;
+    if (this.percentage > 100) {
+      throw new Error("Percentage cannot be greater than 100");
+    }
   }
 }
