c60451a3f800a48dfd1e2472e77dc0ba1596f830
woolnerd <davidwoolner@gmail.com>
2024-04-23 18:56:56 -0400

    add 16bit checks to profile table

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -4,13 +4,15 @@ import journal from './meta/_journal.json';
 import m0000 from './0000_bored_phil_sheldon.sql';
 import m0001 from './0001_watery_gwen_stacy.sql';
 import m0002 from './0002_fuzzy_morlun.sql';
+import m0003 from './0003_flimsy_wild_child.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
 m0001,
-m0002
+m0002,
+m0003
     }
   }
   
