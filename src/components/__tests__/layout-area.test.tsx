ad7819e50bd15e2af9b93d94296c1980a6c30597
woolnerd <davidwoolner@gmail.com>
2024-05-08 19:12:43 -0400

    fix layouy area test, add zustand mock

--- src/components/__tests__/layout-area.test.tsx
+++ src/components/__tests__/layout-area.test.tsx
@@ -4,31 +4,33 @@ import React from "react";
 
 jest.mock("../../util/fixture-cache");
 jest.mock("../../db/client.ts");
+jest.mock("../../app/store/useCompositeFixtureStore.ts");
+jest.mock("../../app/store/useFixtureChannelSelectionStore.ts");
+jest.mock("../../app/store/useManualFixtureStore.ts");
+jest.mock("../../models/scene-to-fixture-assignments");
 
+import { useCompositeFixtureStore } from "../../app/store/useCompositeFixtureStore.ts";
+import { useFixtureChannelSelectionStore } from "../../app/store/useFixtureChannelSelectionStore.ts";
+import { useManualFixtureStore } from "../../app/store/useManualFixtureStore.ts";
 import ScenesToFixtureAssignments from "../../models/scene-to-fixture-assignments.ts";
-import * as fixtureUtils from "../../util/fixture-cache.ts";
 import LayoutArea from "../layout-area.tsx";
 
 describe("LayoutArea", () => {
-  // Setup common props to reuse
-  const commonProps = { selectedSceneId: 1, goToOut: false };
-
-  // Mock data to return from our mocked functions
-  const mockFixtureResponse = [
+  const mockCompositeFixtures = [
     {
       fixtureAssignmentId: 1,
       channel: 1,
-      values: JSON.stringify([
+      values: [
         [1, 100],
         [2, 255],
-      ]),
+      ],
       title: "Fixture 1",
-      profileChannels: JSON.stringify({
+      profileChannels: {
         1: "intensity",
         2: "red",
         3: "green",
         4: "blue",
-      }),
+      },
       profileName: "Profile 1",
       fixtureName: "Fixture 1",
       fixtureNotes: "no notes",
@@ -37,70 +39,102 @@ describe("LayoutArea", () => {
     {
       fixtureAssignmentId: 2,
       channel: 2,
-      values: JSON.stringify([
+      values: [
         [1, 100],
         [2, 255],
-      ]),
+      ],
       title: "Fixture 2",
-      profileChannels: JSON.stringify({
+      profileChannels: {
         1: "intensity",
         2: "red",
         3: "green",
         4: "blue",
-      }),
+      },
       profileName: "Profile 1",
       fixtureName: "Fixture 2",
       fixtureNotes: "no notes",
       sceneId: 1,
+      addressStart: 1,
+      addressEnd: 20,
+      colorTempLow: 2800,
+      colorTempHigh: 10000,
     },
   ];
+
+  const mockManualFixtures = {
+    1: { values: [[1, 255]] },
+  };
 
   beforeEach(() => {
     // Clear all mocks before each test
     jest.clearAllMocks();
+    useCompositeFixtureStore.mockReturnValue({
+      compositeFixturesStore: mockCompositeFixtures,
+      updateCompositeFixturesStore: jest.fn(),
+    });
 
-    // Setup our mocks
-    ScenesToFixtureAssignments.prototype.getCompositeFixtureInfo = jest
-      .fn()
-      .mockResolvedValue(mockFixtureResponse);
-    (fixtureUtils.getManualFixtureKeys as jest.Mock) = jest
-      .fn()
-      .mockResolvedValue([
-        "sceneId:1#fixtureAssignementId:1",
-        "sceneId:1#fixtureAssignementId:2",
-      ]);
-    (fixtureUtils.getAllFixturesFromSceneKeys as jest.Mock) = jest
-      .fn()
-      .mockResolvedValue([
-        {
-          fixtureAssignmentId: 3,
-          channel: 3,
-          values: JSON.stringify([
-            [1, 100],
-            [2, 255],
-          ]),
-          title: "Fixture 1",
-          profileChannels: JSON.stringify({
-            1: "intensity",
-            2: "red",
-            3: "green",
-            4: "blue",
-          }),
-          profileName: "Profile 1",
-          fixtureName: "Fixture 3",
-          fixtureNotes: "no notes",
-          sceneId: 1,
-        },
-      ]);
+    useFixtureChannelSelectionStore.mockReturnValue({
+      fixtureChannelSelectionStore: new Set(),
+    });
+
+    useManualFixtureStore.mockReturnValue({
+      manualFixturesStore: mockManualFixtures,
+    });
+
+    (ScenesToFixtureAssignments as jest.Mock).mockImplementation(() => ({
+      getCompositeFixtureInfo: jest
+        .fn()
+        .mockResolvedValue(mockCompositeFixtures),
+    }));
   });
 
-  it("renders correctly", async () => {
-    const { getByText } = render(<LayoutArea {...commonProps} />);
+  test("renders correctly", async () => {
+    const { getByText } = render(
+      <LayoutArea selectedSceneId={1} goToOut={false} />,
+    );
 
-    // Wait for async operations to complete
     await waitFor(() => {
       expect(getByText("Fixture 1")).toBeTruthy();
-      expect(getByText("Fixture 3")).toBeTruthy();
+      expect(getByText("Fixture 2")).toBeTruthy();
+    });
+  });
+
+  test("fetch and update composite fixtures", async () => {
+    const { updateCompositeFixturesStore } = useCompositeFixtureStore();
+    render(<LayoutArea selectedSceneId={1} goToOut={false} />);
+
+    await waitFor(() =>
+      expect(updateCompositeFixturesStore).toHaveBeenCalledWith(
+        mockCompositeFixtures,
+      ),
+    );
+  });
+
+  test("merge manual and composite fixtures", async () => {
+    const { updateCompositeFixturesStore } = useCompositeFixtureStore();
+    render(<LayoutArea selectedSceneId={1} goToOut={false} />);
+
+    await waitFor(() => {
+      expect(updateCompositeFixturesStore).toHaveBeenCalledWith(
+        expect.arrayContaining([
+          expect.objectContaining({
+            fixtureAssignmentId: 1,
+            channel: 1,
+            values: [
+              [1, 100],
+              [2, 255],
+            ],
+          }),
+          expect.objectContaining({
+            fixtureAssignmentId: 2,
+            channel: 2,
+            values: [
+              [1, 100],
+              [2, 255],
+            ],
+          }),
+        ]),
+      );
     });
   });
 });
