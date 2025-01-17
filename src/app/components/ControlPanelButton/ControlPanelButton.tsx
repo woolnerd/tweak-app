c0dc23fa78e723ce34a3768081c7bb8c5e18da0e
woolnerd <davidwoolner@gmail.com>
2025-01-16 19:19:56 -0500

    remove stylesheets from controlpanelbutton

--- src/app/components/ControlPanelButton/ControlPanelButton.tsx
+++ src/app/components/ControlPanelButton/ControlPanelButton.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, StyleSheet } from "react-native";
+import { Pressable, Text } from "react-native";
 
 import {
   selectionHasColorTemp,
@@ -60,33 +60,13 @@ export default function ControlPanelButton({
       key={buttonData.id}
       accessibilityRole="button"
       testID={`cp-button-${buttonData.label}`}
-      // style={styles.touchpadBtn}
       className={`h-12 m-1 border-gray-700 rounded-md border-2 ${backgroundColorStyle} active:border-yellow-500 `}
       onPress={() => handleTouch(buttonData)}
       disabled={disable}>
       <Text
-        // style={{
-        //   fontSize: 12,
-        //   textAlign: "center",
-        //   padding: 15,
-        //   fontWeight: "800",
-        //   backgroundColor: `${disable ? "gray" : buttonData.styles.color}`,
-        // }}
         className={`text-xs text-inherit text-center p-4 font-extrabold ${styles.font}`}>
         {buttonData.label}
       </Text>
     </Pressable>
   );
 }
-
-// const styles = StyleSheet.create({
-//   touchpadBtn: {
-//     height: 48,
-//     width: "90%",
-//     backgroundColor: "gray",
-//     marginVertical: 2,
-//     borderColor: "blue",
-//     borderWidth: 2,
-//     gap: 2,
-//   },
-// });
